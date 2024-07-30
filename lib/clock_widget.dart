ad1faae63a50ecde07e708fbc47210d83f19bb40
IlyasMuhammedWac <ilyas@wacmob.com>
2024-07-30 16:32:14 +0530

    initial commit

---
+++ lib/clock_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ClockWidget extends StatefulWidget {
+  const ClockWidget({super.key});
+
+  @override
+  State<ClockWidget> createState() => _ClockWidgetState();
+}
+
+class _ClockWidgetState extends State<ClockWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
